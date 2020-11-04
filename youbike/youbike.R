a0a66e004fafc1ef5614f1dbca864105c2c8a9fc
Ching-Chang Lin <cclin@im.tpcu.edu.tw>
2020-11-04 11:06:18 +0800

    group_by

--- youbike/youbike.R
+++ youbike/youbike.R
@@ -7,9 +7,34 @@ library('jsonlite')
 ubike.json <- 'youbike.json'
 download.file('https://quality.data.gov.tw/dq_download_json.php?nid=123026&md5_url=4d8de527a0bcd8a7b1aeae91120f021d',ubike.json)
 ubike <- fromJSON(ubike.json)
+ubike$tot <- as.numeric(ubike$tot) 
 x <- table(ubike$sarea)
 sort(x)
-sort(x,decreasing = T)
+a <- sort(x,decreasing = T)
+b <- as.data.frame(a)
+names(b) <- c('Area','Count')
+write.csv(b,file="NTP.csv",fileEncoding = "UTF-8",row.names = F)
+sum(b$Count)
+boxplot(b$Count)
+
+
+library(dplyr)
+ubike$tot <- as.numeric(ubike$tot) 
+c <- ubike %>%
+  group_by(sarea) %>%
+  summarise(across(tot, sum)) %>%
+  arrange(desc(tot))
+
+boxplot(c$tot)
+
+#[How to sum a variable by group](https://stackoverflow.com/questions/1660124/how-to-sum-a-variable-by-group)
+
+DF <-  data.frame(Categ1=factor(c('A','A','B','B','A','B','A')),
+             Categ2=factor(c('X','Y','X','X','X','Y','Y')),
+             Samples=c(1,2,4,3,5,6,7),
+             Freq=c(10,30,45,55,80,65,50))
+
+DF %>% group_by(Categ1,Categ2) %>% summarise(across(Freq, sum))
 
 # Taipei Youbike
 install.packages('data.table')
